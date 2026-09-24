ee887c9a05016f661985ecc0af4a6d7f9993691a
cursosonline-recursos <megacursosonline.soporte@gmail.com>
2026-09-23 21:09:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Never put the Supabase service-role key or Hotmart Hottok in this file.
 window.ACADEMY_CONFIG = {
   supabaseUrl: "https://ixdbxdvslnrhgmjsgzmn.supabase.co",
-  supabaseAnonKey: "",
+  supabaseAnonKey: "sb_publishable_T6-DvTQvQgr70CLvDaIkmw_owSW_rjA",
   hotmartCheckoutUrl: ""
 };
